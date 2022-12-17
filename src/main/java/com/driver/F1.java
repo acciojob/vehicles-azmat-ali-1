4aecfb0b7979829271d71d577fab14f4307a726b
AZMAT ALI <222azmat@gmail.com>
2022-12-17 15:21:01 +0530

    $

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -31,43 +31,44 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
+        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
             changeGear(1);
-            System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
             stop();
+
         }
         //for all other cases, change the gear accordingly
 
         else if(newSpeed >=1 &&50>=newSpeed) {
             changeGear(1);
-            System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
+            //System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
 
         }
         else if(newSpeed >=51 &&100>=newSpeed) {
             changeGear(2);
-            System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
+           // System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
 
         }
         else if(newSpeed >=101 &&150>=newSpeed) {
             changeGear(3);
-            System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
+            //System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
 
         }
         else if(newSpeed >=151 &&200>=newSpeed) {
             changeGear(4);
-            System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
+            //System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
 
         }
         else if(newSpeed >=201 &&250>=newSpeed) {
             changeGear(5);
-            System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
+            //System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
 
         }
         else if(250<newSpeed) {
             changeGear(6);
-            System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
+            //System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed);
 
         }
 
